279346d623d4a5f53168626e28e7fee491ee9120
Stephanie Pena <stephanie.pena@stephanies-mbp.myfiosgateway.com>
2020-10-08 22:53:23 -0400

    problem 2

--- Week3_Loops_Project/Question_2_WhileLoop_ArrayOddNumsInRange.js
+++ Week3_Loops_Project/Question_2_WhileLoop_ArrayOddNumsInRange.js
@@ -1,14 +1,11 @@
 //Using a while loop, return an array that contains all odd numbers between 3 and 103
-while (i > 3 && i < 103) {
-  let i = [];
-    if (i % 2 !== 0) {
-      return [i];
-    }
+
+let startingNum = 3;
+let endingNum = 103;
+let oddNumArr = [];
+
+while (startingNum < endingNum) {
+  oddNumArr.push(startingNum);
+  startingNum += 2;
 }
-
-
-let arr = [1, 2, 3, 4];
-const evenResult = arr.filter(function (newArr) {
-  return newArr % 2 == 0;
-});
-console.log(evenResult);
+console.log(oddNumArr);
