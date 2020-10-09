c7c7c2b66b346d67c0ea5e089e3f08262a62f199
Stephanie Pena <stephanie.pena@stephanies-mbp.myfiosgateway.com>
2020-10-09 02:00:13 -0400

    problem 3

--- Week3_Loops_Project/Question_3.js
+++ Week3_Loops_Project/Question_3.js
@@ -1,5 +1,12 @@
-//arraySum(num) receives an array of numbers as an argument and returns the sum of all numbers,  for example: arraySum([1, 2, 3, 4, 5, 6]) => 21, use any loop. 
+//arraySum(num) receives an array of numbers as an argument and returns the sum of all numbers,  for example: arraySum([1, 2, 3, 4, 5, 6]) => 21, use any loop.
 
-function arraySum(num)
-  let 
+function arraySum(num) {
+  let numSum = 0;
 
+  for (let i = 0; i < num.length; i++) {
+    numSum += parseInt(num[i]);
+  }
+  return numSum;
+}
+
+arraySum([1, 2, 3, 4, 5, 6]);
