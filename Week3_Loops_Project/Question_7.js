c18227563e40f62decfed05f9adf0c8cd1382b62
Stephanie Pena <stephanie.pena@stephanies-mbp.myfiosgateway.com>
2020-10-09 00:06:14 -0400

    problem 7 updated

--- Week3_Loops_Project/Question_7.js
+++ Week3_Loops_Project/Question_7.js
@@ -1,43 +1,19 @@
-function secondLetterZ (givenString) {
-  // string to array
-  let stringArray = givenString.split("")
-​
-  // change every 2nd letter to "Z"
+function secondLetterZ(givenString) {
+  // change the string to array, splits at every letter
+  let stringArray = givenString.split("");
+
+  // change every 2nd letter (index must be 1, 3, 5 etc) to "Z" (must be Uppercase)
   let mapZString = stringArray.map((letter, index) => {
     if (index % 2 === 1) {
-     return letter = "Z"; //uppercase Z
+      return (letter = "Z");
     } else {
       return letter.toLowerCase();
     }
-  })
-​
+  });
+  // piece back together letters into a string
   let noSpaceString = mapZString.join("");
-​
-  return noSpaceString;
-}
-​
-secondLetterZ("donuts")
 
-
-//
-function letterZ (str) {
-  // change string to array
-  let strArr = str.split("")
-​
-  // change every 2nd letter to "Z"
-  let mappedStr = strArr.map((letter, index) => {
-    if (index % 2 === 1) {
-     return letter = "Z";
-    } else {
-      return letter.toLowerCase();
-    }
-  })
-​
-  // join array back into string
-  let zeeStr = mappedStr.join("");
-​
-  // return it
-  return zeeStr;
+  return noSpaceString; //resutling string with "t"s changed to "Z"s
 }
-​
-letterZ("donuts")
+
+console.log(secondLetterZ("donuts"));
