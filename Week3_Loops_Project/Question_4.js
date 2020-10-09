946b53b3df425a6a45b72fdcf794cc7d856435aa
Stephanie Pena <stephanie.pena@stephanies-mbp.myfiosgateway.com>
2020-10-09 02:12:55 -0400

    problem 4

--- Week3_Loops_Project/Question_4.js
+++ Week3_Loops_Project/Question_4.js
@@ -1,13 +1,3 @@
 //Use a loop to display the numbers 9 to 0 in descending order.
-let arrAscending = [];
-for (let i = 1; i < 10; i++)
-  arrAscending = 
-  
-let sorted = [3, 1, 6, 2].sort(function(a, b){
-  return b - a;
-}); // sort descending 
-  
-  console.log(i);
 
-
-
+for (let i = 9; i >= 0; i--) console.log(i);
